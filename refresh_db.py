9b0abd4e84aa0af326291a58ddbeb8a7f85f7abd
Joey Lafyatis <joeylafyatis@gmail.com>
2019-10-29 16:50:33 -0400

    did like... 100 things

--- refresh_db.py
+++ refresh_db.py
@@ -5,58 +5,69 @@ import requests
 import sqlite3
 
 NHL_API = 'https://statsapi.web.nhl.com/api/v1/{}'
-[CURRENT_SEASON] = requests.get(NHL_API.format('seasons/current')).json()['seasons']
 NHL_DB = sqlite3.connect('nhl.db')
+
+standard_get = lambda x: requests.get(NHL_API.format(x)).json()
+# concat_df = lambda x: pd.concat([ pd.io.json.json_normalize(x)
+
+# def standard_get(base,modifier):
+#     r = requests.get(NHL_API.format(base)).json()
+#     #incorporate modifier?
 
 def refresh_table(df_source,db_table):
     with open('table_specs.json') as f:
         data = json.load(f)[db_table]
     df_source = df_source[data['select_columns']]
     df_source.columns = data['rename_columns']
-    df_source.to_sql(name=db_table,con=NHL_DB,if_exists='replace',index=0,dtype=data['cast_dtypes'])
+    df_source.to_sql(
+        name=db_table,
+        con=NHL_DB,
+        if_exists='replace',
+        index=0,
+        dtype=data['cast_dtypes']
+    )
 
 def standard_refresh(endpoint):
-    r = requests.get(NHL_API.format(endpoint)).json()[endpoint]
+    r = standard_get(endpoint)[endpoint]
     df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=1)
     refresh_table(df,endpoint[:-1])
 
 def refresh_players():
-    # BEGIN GARBAGE
-    c = NHL_DB.cursor().execute('SELECT team_id FROM team;')
-    rosters = [ 'teams/{}?expand=team.roster'.format(x[0]) for x in c.fetchall() ]
-    get_rosters = [ requests.get(NHL_API.format(x)).json()['teams'][0]['roster']['roster'] for x in rosters ]
+    team_ids = NHL_DB.cursor().execute('SELECT team_id FROM team;')
+    api_template = 'teams/{}?expand=team.roster'
+    rosters = [ api_template.format(t[0]) for t in team_ids.fetchall() ]
+    get_rosters = [ standard_get(x)['teams'][0]['roster']['roster'] for x in rosters ]
     df_rosters = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_rosters ],sort=1)
 
     player_ids = df_rosters['person_id'].tolist()
     players = [ 'people/{}'.format(x) for x in player_ids ]
-    get_players = [ requests.get(NHL_API.format(x)).json()['people'] for x in players ]
+    get_players = [ standard_get(x)['people'] for x in players ]
     df_players = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_players ],sort=1)
-    # END GARBAGE
     refresh_table(df_players,'player')
 
 def refresh_games():
-    endpoint = 'schedule?startDate={regularSeasonStartDate}&endDate={regularSeasonEndDate}'.format( **CURRENT_SEASON)
-    get_games = requests.get(NHL_API.format(endpoint)).json()['dates']
+    current_season = standard_get('seasons/current')['seasons']
+    endpoint = 'schedule?\
+        startDate={regularSeasonStartDate}&\
+        endDate={regularSeasonEndDate}'.format( **current_season)
+    get_games = standard_get(endpoint)['dates']
     df_games = pd.concat([ pd.io.json.json_normalize(x['games'],sep='_') for x in get_games ],sort=1)
     refresh_table(df_games,'game')
 
 def refresh_gamelogs():
-    # BEGIN GARBAGE
     c = NHL_DB.cursor().execute('SELECT player_id FROM player;')
     df_gamelogs = pd.DataFrame()
     for x in c.fetchall():
         player_id = x[0]
         api = 'people/{}/stats?stats=gameLog&season=20192020'.format(player_id)
-        get_stats = requests.get(NHL_API.format(api)).json()['stats'][0]['splits']
+        get_stats = standard_get(api)['stats'][0]['splits']
         df_stats = pd.io.json.json_normalize(get_stats,sep='_')
         df_stats['player_id'] = player_id
         df_gamelogs = df_gamelogs.append(df_stats,sort=1)
-    # END GARBAGE
     refresh_table(df_gamelogs,'gamelog')
 
 def refresh_standings():
-    # BEGIN GARBAGE
-    get_standings = requests.get(NHL_API.format('standings')).json()['records']
+    get_standings = standard_get('standings')['records']
     df_standings = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_standings ],sort=1)
     df_standings = df_standings[['division_id','teamRecords']]
 
@@ -64,7 +75,6 @@ def refresh_standings():
     df_standings = pd.DataFrame(data=flatten_records[0],columns=df_standings.columns)
     for x in ['teamRecords','team','leagueRecord','streak']:
         df_standings = pd.concat([df_standings,df_standings[x].apply(pd.Series)],axis=1)
-    # END GARBAGE
     refresh_table(df_standings,'standings')
 
 def main():
