e27e6a2a9fc35295f29434e8a8fd8b4bb550c803
Joey Lafyatis <joeylafyatis@gmail.com>
2019-10-29 16:15:24 -0400

    added def standard_refresh()

--- refresh_db.py
+++ refresh_db.py
@@ -8,72 +8,67 @@ NHL_API = 'https://statsapi.web.nhl.com/api/v1/{}'
 [CURRENT_SEASON] = requests.get(NHL_API.format('seasons/current')).json()['seasons']
 NHL_DB = sqlite3.connect('nhl.db')
 
-def refresh_table(df,table_spec):
+def refresh_table(df_source,db_table):
     with open('table_specs.json') as f:
-        data = json.load(f)[table_spec]
-    df = df[data['select_columns']]
-    df.columns = data['rename_columns']
-    df.to_sql(name=table_spec,con=NHL_DB,if_exists='replace',index=False,dtype=data['cast_columns'])
+        data = json.load(f)[db_table]
+    df_source = df_source[data['select_columns']]
+    df_source.columns = data['rename_columns']
+    df_source.to_sql(name=db_table,con=NHL_DB,if_exists='replace',index=0,dtype=data['cast_dtypes'])
 
-def refresh_conferences():
-    r = requests.get(NHL_API.format('conferences')).json()['conferences']
-    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=True)
-    refresh_table(df,'conference')
-
-def refresh_divisions():
-    r = requests.get(NHL_API.format('divisions')).json()['divisions']
-    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=True)
-    refresh_table(df,'division')
-
-def refresh_teams():
-    r = requests.get(NHL_API.format('teams')).json()['teams']
-    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=True)
-    refresh_table(df,'team')
+def standard_refresh(endpoint):
+    r = requests.get(NHL_API.format(endpoint)).json()[endpoint]
+    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=1)
+    refresh_table(df,endpoint[:-1])
 
 def refresh_players():
+    # BEGIN GARBAGE
     c = NHL_DB.cursor().execute('SELECT team_id FROM team;')
     rosters = [ 'teams/{}?expand=team.roster'.format(x[0]) for x in c.fetchall() ]
-    r1 = [ requests.get(NHL_API.format(x)).json()['teams'][0]['roster']['roster'] for x in rosters ]
-    df_rosters = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r1 ],sort=True)
+    get_rosters = [ requests.get(NHL_API.format(x)).json()['teams'][0]['roster']['roster'] for x in rosters ]
+    df_rosters = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_rosters ],sort=1)
+
     player_ids = df_rosters['person_id'].tolist()
     players = [ 'people/{}'.format(x) for x in player_ids ]
-    r2 = [ requests.get(NHL_API.format(x)).json()['people'] for x in players ]
-    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r2 ],sort=True)
-    refresh_table(df,'player')
+    get_players = [ requests.get(NHL_API.format(x)).json()['people'] for x in players ]
+    df_players = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_players ],sort=1)
+    # END GARBAGE
+    refresh_table(df_players,'player')
 
 def refresh_games():
     endpoint = 'schedule?startDate={regularSeasonStartDate}&endDate={regularSeasonEndDate}'.format( **CURRENT_SEASON)
-    r = requests.get(NHL_API.format(endpoint)).json()['dates']
-    df = pd.concat([ pd.io.json.json_normalize(x['games'],sep='_') for x in r ],sort=True)
-    refresh_table(df,'game')
+    get_games = requests.get(NHL_API.format(endpoint)).json()['dates']
+    df_games = pd.concat([ pd.io.json.json_normalize(x['games'],sep='_') for x in get_games ],sort=1)
+    refresh_table(df_games,'game')
 
 def refresh_gamelogs():
+    # BEGIN GARBAGE
     c = NHL_DB.cursor().execute('SELECT player_id FROM player;')
-    df = pd.DataFrame()
+    df_gamelogs = pd.DataFrame()
     for x in c.fetchall():
         player_id = x[0]
         api = 'people/{}/stats?stats=gameLog&season=20192020'.format(player_id)
-        r = requests.get(NHL_API.format(api)).json()['stats'][0]['splits']
-        df_playerstats = pd.io.json.json_normalize(r,sep='_')
-        df_playerstats['player_id'] = player_id
-        df = df.append(df_playerstats,sort=True)
-    refresh_table(df,'gamelog')
+        get_stats = requests.get(NHL_API.format(api)).json()['stats'][0]['splits']
+        df_stats = pd.io.json.json_normalize(get_stats,sep='_')
+        df_stats['player_id'] = player_id
+        df_gamelogs = df_gamelogs.append(df_stats,sort=1)
+    # END GARBAGE
+    refresh_table(df_gamelogs,'gamelog')
 
 def refresh_standings():
-    r = requests.get(NHL_API.format('standings')).json()['records']
-    df = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in r ],sort=True)
-    df = df[['division_id','teamRecords']]
+    # BEGIN GARBAGE
+    get_standings = requests.get(NHL_API.format('standings')).json()['records']
+    df_standings = pd.concat([ pd.io.json.json_normalize(x,sep='_') for x in get_standings ],sort=1)
+    df_standings = df_standings[['division_id','teamRecords']]
 
-    flatten_records = np.dstack((np.repeat(df.division_id.values,list(map(len,df.teamRecords.values))),np.concatenate(df.teamRecords.values)))
-    df = pd.DataFrame(data=flatten_records[0],columns=df.columns)
+    flatten_records = np.dstack((np.repeat(df_standings.division_id.values,list(map(len,df_standings.teamRecords.values))),np.concatenate(df_standings.teamRecords.values)))
+    df_standings = pd.DataFrame(data=flatten_records[0],columns=df_standings.columns)
     for x in ['teamRecords','team','leagueRecord','streak']:
-        df = pd.concat([df,df[x].apply(pd.Series)],axis=1)
-    refresh_table(df,'standings')
+        df_standings = pd.concat([df_standings,df_standings[x].apply(pd.Series)],axis=1)
+    # END GARBAGE
+    refresh_table(df_standings,'standings')
 
 def main():
-    refresh_conferences()
-    refresh_divisions()
-    refresh_teams()
+    [ standard_refresh(x) for x in ['conferences', 'divisions', 'teams'] ]
     refresh_players()
     refresh_games()
     refresh_gamelogs()
