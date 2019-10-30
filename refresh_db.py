056e00bbdbe7b911a0e0426d872afd28438222c4
Joey Lafyatis <joeylafyatis@gmail.com>
2019-10-30 05:27:13 -0400

    cleaned up standings, new svg files

--- refresh_db.py
+++ refresh_db.py
@@ -24,7 +24,7 @@ def refresh_table(table,spec,df):
     print('Finished table refresh for {}.'.format(table))
 
 def request_api(table,spec):
-    print('Accessing NHL API for {}...'.format(table))
+    print('Accessing NHL API data for {}...'.format(table))
     if spec['standard_refresh']:
         endpoint = spec['api_endpoint']
         get = lambda_get(endpoint)[endpoint]
@@ -40,8 +40,9 @@ def request_api(table,spec):
     return { 'table': table, 'spec': spec, 'df': df }
 
 def refresh_player(table,spec):
-    team_ids = NHL_DB.cursor().execute('SELECT team_id FROM team;').fetchall()
-    endpoints = [ 'teams/{}?expand=team.roster'.format(t[0]) for t in team_ids ]
+    sql = 'SELECT team_id FROM team;'
+    team_ids = [ t[0] for t in NHL_DB.cursor().execute(sql).fetchall() ]
+    endpoints = [ 'teams/{}?expand=team.roster'.format(t) for t in team_ids ]
     rosters = [ lambda_get(e)['teams'][0]['roster']['roster'] for e in endpoints ]
     df_rosters = lambda_df(rosters)
 
@@ -57,32 +58,27 @@ def refresh_game(table,spec):
     return lambda_df(games)
 
 def refresh_gamelog(table,spec):
-    player_ids = NHL_DB.cursor().execute('SELECT player_id FROM player;')
+    sql = 'SELECT player_id FROM player;'
+    player_ids = [ p[0] for p in NHL_DB.cursor().execute(sql).fetchall() ]
     df_gamelogs = pd.DataFrame()
-    for x in player_ids.fetchall():
-        player_id = x[0]
-        api = spec['api_endpoint'].format(player_id)
+    for p in player_ids:
+        api = spec['api_endpoint'].format(p)
         get_stats = lambda_get(api)['stats'][0]['splits']
         df_stats = pd.io.json.json_normalize(get_stats,sep='_')
-        df_stats['player_id'] = player_id
+        df_stats['player_id'] = p
         df_gamelogs = df_gamelogs.append(df_stats,sort=1)
     return df_gamelogs
 
-    # endpoints = [ spec['api_endpoint'].format(p[0]) for p in player_ids.fetchall() ]
+    # sql = 'SELECT player_id FROM player;'
+    # player_ids = [ p[0] for p in NHL_DB.cursor().execute(sql).fetchall() ]
+    # endpoints = [ spec['api_endpoint'].format(p) for p in player_ids ]
     # gamelogs = [ lambda_get(e)['stats'][0]['splits'] for e in endpoints ]
-    # df_gamelogs = lambda_df(stats)
+    # df_gamelogs = lambda_df(gamelogs)
     # ...
 
 def refresh_standings(table,spec):
     standings = lambda_get(spec['api_endpoint'])['records']
-    df_standings = lambda_df(standings)[['division_id','teamRecords']]
-
-    # BEGIN GARBAGE
-    flatten_records = np.dstack((np.repeat(df_standings.division_id.values,list(map(len,df_standings.teamRecords.values))),np.concatenate(df_standings.teamRecords.values)))
-    df_standings = pd.DataFrame(data=flatten_records[0],columns=df_standings.columns)
-    for x in ['teamRecords','team','leagueRecord','streak']:
-        df_standings = pd.concat([df_standings,df_standings[x].apply(pd.Series)],axis=1)
-    # END GARBAGE
+    df_standings = pd.concat([ lambda_df(s['teamRecords']) for s in standings ])
     return df_standings
 
 def main():
